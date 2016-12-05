d4836ce7da5a082473a47dc3e9260ee00dcf2a56
Chouquette <nbheasel@gmail.com>
2016-12-05 17:25:25 +0100

    SEE MY FILE

--- controllers/promo.php
+++ controllers/promo.php
@@ -1,6 +1,27 @@
 <?php
 
 	function promo_choix(){
+
+        require_once("db_connect.php");
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $db_name);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
+
+        if($stmt = $conn->prepare("SELECT code_promo, libelle FROM promo")){
+            $stmt->execute();
+            $stmt->bind_result($code_promo, $libelle);
+            while($stmt->fetch()){
+                printf("%s je teste %s/n", $code_promo, $libelle);
+            }
+            $stmt->close();
+        }
+        $mysqli->close();
         return html('promo.php','layout/layout.php');
     }
 
