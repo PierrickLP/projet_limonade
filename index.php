4c60548ac1818bfa6e39da5b36f66d801e5f04af
Chouquette <nbheasel@gmail.com>
2016-12-05 15:15:23 +0100

    Pierrick dit qu'on s'en fous mais je le fais quand même

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 	require_once('lib/limonade.php');
 
 	dispatch('/accueil','accueil');
-	// dispatch('/promo','promo_choix');
+	dispatch('/promo','promo_choix');
 	// dispatch('/promo/ajout','promo_ajout');
 	// dispatch('/promo/modif','promo_modif');
-	// dispatch('/fichier','fichier_choix');
+	dispatch('/fichier','fichier_choix');
 	// dispatch('/fichier/ajout','fichier_ajout');
 	// dispatch('/fichier/modif','fichier_modif');
 
