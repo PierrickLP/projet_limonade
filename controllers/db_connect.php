b22134280f1794d26008b3f06e72fa5c73947bae
Chouquette <nbheasel@gmail.com>
2016-12-05 17:24:08 +0100

    SEE MY FILE

--- controllers/db_connect.php
+++ controllers/db_connect.php
@@ -2,12 +2,5 @@
 $servername = "localhost";
 $username = "cir32016";
 $password = "cir32016";
+$db_name = "doc_rentree";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
